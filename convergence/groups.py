ab601c0698e27f6d02d017549874d8bbb8d1ce47
Daniel <daniel@hgrsd.nl>
2019-06-20 23:16:41 +0100

    Ref issue #15 - Improved database queries efficiency

--- convergence/groups.py
+++ convergence/groups.py
@@ -79,13 +79,11 @@ def remove_user_from_group(request_id, user_id, group_id):
     :param group_id: group to delete user from
     :return: dict object with body and status code
     """
-    if not Group.query.get(group_id):
-        return {"body": {"error": {"message": "group does not exist"}}, "status_code": 400}
-    if not User.query.get(user_id):
-        return {"body": {"error": {"message": "user does not exist"}}, "status_code": 400}
+    to_delete = UserGroup.query.filter_by(user_id=user_id, group_id=group_id).first()
+    if not to_delete:
+        return {"body": {"error": {"message": "invalid group or member"}}, "status_code": 400}
     if not Group.query.get(group_id).owner == request_id:
         return {"body": {"error": {"message": "permission denied"}}, "status_code": 400}
-    to_delete = UserGroup.query.filter_by(user_id=user_id, group_id=group_id).first()
     db.session.delete(to_delete)
     try:
         db.session.commit()
@@ -102,16 +100,13 @@ def get_members(request_id, group_id):
     :param group_id: group of which members are requested
     :return: dict object with body and status code
     """
-    if not group_id:
-        return {"body": {"error": {"message": "no group_id specified"}}, "status_code": 400}
-    if not Group.query.get(group_id):
-        return {"body": {"error": {"message": "group does not exist"}}, "status_code": 400}
     if not UserGroup.query.filter_by(user_id=request_id, group_id=group_id):
         return {"body": {"error": {"message": "access denied"}}, "status_code": 400}
-    entries = UserGroup.query.filter_by(group_id=group_id).all()
-    if not entries:
+    users = db.session.query(User).join(UserGroup, User.id == UserGroup.user_id) \
+            .filter(UserGroup.group_id == group_id) \
+            .all()
+    if not users:
         return {"body": {"status": "success", "data": []}, "status_code": 200}
-    users = [User.query.get(entry.user_id) for entry in entries]
     return {"body": {"status": "success", "data": [user.basic_info() for user in users]}, "status_code": 200}
 
 
@@ -135,21 +130,14 @@ def get_available_members(request_id, group_id):
     :param group_id: group of which members are requested
     :return: dict object with body and status code
     """
-    if not group_id:
-        return {"body": {"error": {"message": "no group_id specified"}}, "status_code": 400}
-    if not Group.query.get(group_id):
-        return {"body": {"error": {"message": "group does not exist"}}, "status_code": 400}
     if not UserGroup.query.filter_by(user_id=request_id, group_id=group_id):
         return {"body": {"error": {"message": "access denied"}}, "status_code": 400}
-    entries = UserGroup.query.filter_by(group_id=group_id).all()
-    available_users = []
-    for entry in entries:
-        user = User.query.filter_by(id=entry.user_id, available=True).first()
-        if user:
-            available_users.append(user)
-    if not available_users:
+    users = db.session.query(User).join(UserGroup, User.id == UserGroup.user_id) \
+            .filter(UserGroup.group_id == group_id, User.available == True) \
+            .all()
+    if not users:
         return {"body": {"status": "success", "data": []}, "status_code": 200}
-    return {"body": {"status": "success", "data": [user.basic_info() for user in available_users]}, "status_code": 200}
+    return {"body": {"status": "success", "data": [user.basic_info() for user in users]}, "status_code": 200}
 
 
 def get_groups(user_id):
@@ -158,8 +146,9 @@ def get_groups(user_id):
     :param user_id: user requesting operation
     :return: dict object with body and status code
     """
-    user_groups = UserGroup.query.filter_by(user_id=user_id)
-    groups = [Group.query.get(user_group.group_id) for user_group in user_groups]
+    groups = db.session.query(Group).join(UserGroup, Group.id == UserGroup.group_id) \
+            .filter(UserGroup.user_id == user_id) \
+            .all()
     if not groups:
         return {"body": {"status": "success", "data": {"groups": []}}, "status_code": 200}
     return {"body": {"status": "success", "data": [group.as_dict() for group in groups]}, "status_code": 200}
